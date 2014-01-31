9d0328e43a03b2be5c39d16aedf2cbf6f676ee75
Philip Saad <philipsaad@gmail.com>
2014-01-30 19:26:22 -0800

    Expanded use of HTML5 download attribute

--- src/script.js
+++ src/script.js
@@ -13,20 +13,16 @@ function InjectControls() {
 		var fullArtistName = $("#trackInfo .trackinfo-p ").first().text();
 		var byArtistText = $("#trackInfo .trackinfo-p span").first().text();
 
-		var artistName = fullArtistName.replace(byArtistText, '').trim().replace(/[\\/:*?:/"<>|]/, '');
-		var trackName = $("#trackInfo h3").text().trim().replace(/[\\/:*?/"<>|]/, '');
+		var artistName = fullArtistName.replace(byArtistText, '').trim().replace(/[\\/:*?:/"<>|]/, '').replace(/'/g, '&#39;');
+		var trackName = $("#trackInfo h3").text().trim().replace(/[\\/:*?/"<>|]/, '').replace(/'/g, '&#39;');
 		var mp3Link = $("#jp_audio_0")[0].currentSrc;
-		var mp3ID = mp3Link.substring(mp3Link.lastIndexOf(".mp3"), mp3Link.lastIndexOf("/") + 1);
-
+		var prettyMp3Link = artistName + ' - ' + trackName + '.mp3';
+		
 		//Optional formatting comment out what you don't like
-		//artistName = artistName.toLowerCase() 
-		//artistName = artistName.replace(/[\s]/g, '_') // Replace spaces with underscores
-		//trackName = trackName.toLowerCase() 
-		//trackName = trackName.replace(/[\s]/g, '_') // Replace spaces with underscores
+		//prettyMp3Link = prettyMp3Link.toLowerCase() 
+		//prettyMp3Link = prettyMp3Link.replace(/[\s]/g, '_') // Replace spaces with underscores
 
-		var prettyMp3Link = mp3Link.replace(mp3ID, escape(artistName) + ' - ' + escape(trackName));
-
-		$("#trackInfo").append("<span id='downloadLink' class='p-fontHighlight trackinfo-p__span'>&#x25BC</span> <a href='" + prettyMp3Link + "' download>Download this song</a>");
+		$("#trackInfo").append("<span id='downloadLink' class='p-fontHighlight trackinfo-p__span'>&#x25BC</span> <a href='" + mp3Link + "' download='" + prettyMp3Link + "'>Download this song</a>");
 
 		$("#skipButton").unbind();
 		$("#skipButton").click(function (event) {
